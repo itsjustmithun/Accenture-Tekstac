53bd2139f3076d3fb3660cfa85aa4c439a82ccf9
itsjustmithun <mitbusiness15@gmail.com>
2020-08-31 18:46:25 +0530

    Area

---
+++ Java/Area Calculation - abstract class/Circle.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+public class Circle extends Shape{
+    private float radius;
+    public Circle(float radius){
+        this.radius=radius;
+    }
+    public double calculateArea(){
+        double a=(double)radius*radius*Math.PI;
+        return a;
+    }
+}
