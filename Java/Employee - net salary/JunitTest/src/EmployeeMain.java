f84bc563963fd241b3a3d02c96f8c5c5708ddbe6
itsjustmithun <mitbusiness15@gmail.com>
2020-09-16 16:02:50 +0530

    Salary

---
+++ Java/Employee - net salary/JunitTest/src/EmployeeMain.java
@@ -0,0 +1,7 @@
+public class EmployeeMain{
+    public static void main(String args[]){
+        EmployeeTest e=new EmployeeTest();
+        e.testCalNetPay1();
+        e.testCalNetPay2();
+    }
+}
