626e2ee6b92d231c362881023a5d0e70c0c13de3
Mithun <mitbusiness15@gmail.com>
2020-11-23 12:42:00 +0530

    quarters

---
+++ Python/Create Quarters/quarters.py
@@ -0,0 +1,21 @@
+months=('January','February','March','April','May','June','July','August','September','October','November','December')
+month1=months[0:3]
+month2=months[3:6]
+month3=months[6:9]
+month4=months[9:12]
+print("Months in expanded form:")
+for i in months:
+    print(i)
+print("The four quarters are:")
+print("First Quarter :")
+for j in month1:
+    print(j)
+print("Second Quarter :")
+for k in month2:
+    print(k)
+print("Third Quarter :")
+for m in month3:
+    print(m)
+print("Fourth Quarter :")
+for n in month4:
+    print(n)
